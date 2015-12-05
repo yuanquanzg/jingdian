e66d2ae66fa6cf129e94dc3752120d228ecb2d9d
ZZG <ZZG@ZGangdeMacBook-Pro.local>
2015-12-05 10:41:52 +0800

    Recreat and push

---
+++ 景点助手/Classes/Other/ZGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZGAppDelegate.h
+//  景点助手
+//
+//  Created by 赵志刚 on 15/11/18.
+//  Copyright © 2015年 赵志刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZGAppDelegate  : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 景点助手/Classes/Other/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  景点助手
+//
+//  Created by 赵志刚 on 15/11/18.
+//  Copyright © 2015年 赵志刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ZGAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZGAppDelegate class]));
+    }
+}
